2b4a878e853a3afd8c354faac632b03503fb941a
cxhacker1 <85056904+cxhacker1@users.noreply.github.com>
2026-05-25 05:53:28 +0800

    更新 dev_init.sh

--- trunk/user/scripts/dev_init.sh
+++ trunk/user/scripts/dev_init.sh
@@ -50,6 +50,7 @@ mkdir -p -m 700 /home/root
 mkdir -p -m 700 /home/root/.ssh
 mkdir -p -m 755 /etc/storage
 mkdir -p -m 755 /etc/ssl
+mkdir -p -m 755 /etc/ssl/certs
 mkdir -p -m 755 /etc/Wireless
 mkdir -p -m 750 /etc/Wireless/RT2860
 mkdir -p -m 750 /etc/Wireless/iNIC
@@ -63,6 +64,9 @@ if [ -f /etc_ro/openssl.cnf ]; then
 	cp -f /etc_ro/openssl.cnf /etc/ssl
 fi
 
+if [ -f /etc_ro/ca-certificates.crt ]; then
+ cp -f /etc_ro/ca-certificates.crt /etc/ssl/cert.pem
+fi
 # create symlinks
 ln -sf /home/root /home/admin
 ln -sf /proc/mounts /etc/mtab
