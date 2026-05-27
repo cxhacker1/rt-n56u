6da28258da4bf76399183b24ec91da2da0d1178e
cxhacker1 <85056904+cxhacker1@users.noreply.github.com>
2026-05-27 23:15:53 +0800

    更新 state.js

--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -473,7 +473,7 @@ menuL1_title = new Array("", "<#menu1#>", "", "", "", "<#menu4#>", "<#menu5_8#>"
 menuL1_link = new Array("", "index.asp", "", "", "", "Main_TrafficMonitor_realtime.asp", "Advanced_System_Info.asp", "as.asp");
 menuL1_icon = new Array("", "icon-home", "", "", "", "icon-tasks", "icon-random", "icon-wrench");
 
-if (found_app_vpnwebui()){
+if (found_app_openvpn()){
 	menuL1_title[3] = "<#menu2#>";
 	menuL1_link[3] = "vpnsrv.asp";
 	menuL1_icon[3] = "icon-retweet";
